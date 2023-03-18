13f4b01b2c0cffc8b34de74d7522928d4e90412f
miasK3011 <neemiascalebe2014@gmail.com>
2023-03-18 12:29:28 -0300

    fixes

--- doom.cpp
+++ doom.cpp
@@ -85,7 +85,6 @@ void renderScene(void) {
 
 void processSpecialKeys(int key, int xx, int yy) {
 
-	float fraction = VEL_MOV;
 	enum {up, down, left, right};
 
 	switch (key) {
@@ -112,49 +111,32 @@ void processNormalKeys(unsigned char key, int x, int y) {
 
 void changeSize(int w, int h) {
 
-	// Prevent a divide by zero, when window is too short
-	// (you cant make a window of zero width).
 	if(h == 0)
 		h = 1;
 	float ratio = 1.0* w / h;
 
-	// Use the Projection Matrix
 	glMatrixMode(GL_PROJECTION);
-
-        // Reset Matrix
 	glLoadIdentity();
 
-	// Set the viewport to be the entire window
 	glViewport(0, 0, w, h);
-
-	// Set the correct perspective.
 	gluPerspective(45,ratio,1,1000);
-
-	// Get Back to the Modelview
 	glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char **argv) {
-
-	// init GLUT and create window
-
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100,100);
 	glutInitWindowSize(320,320);
-	glutCreateWindow("Lighthouse3D - GLUT Tutorial");
+	glutCreateWindow("Doom");
 
-	// register callbacks
 	glutDisplayFunc(renderScene);
 	glutReshapeFunc(changeSize);
 	glutIdleFunc(renderScene);
 	glutKeyboardFunc(processNormalKeys);
 	glutSpecialFunc(processSpecialKeys);
 
-	// OpenGL init
 	glEnable(GL_DEPTH_TEST);
-
-	// enter GLUT event processing cycle
 	glutMainLoop();
 
 	return 1;
