b0fecdcb9d2ee50f25295296d1d6bbe079ff2fc3
miasK3011 <neemiascalebe2014@gmail.com>
2023-03-21 15:04:01 -0300

    Add: Variaveis de janela

---
+++ Assets/config.h
@@ -0,0 +1,16 @@
+/*
+    Biblioteca com configurações do game:
+    VEL_MOV = Velocidade de movimento do jogador
+    SENSE_CAM = Sensibilidade da camera
+
+*/
+
+#ifndef config_h
+#define config_h
+
+#define JANELA_X 640
+#define JANELA_Y 480
+#define VEL_MOV 0.6
+#define SENSE_CAM 0.06
+
+#endif
