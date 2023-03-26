4cb69176f84a42ed1d6e7628e571c7841bf3f87d
miasK3011 <neemiascalebe2014@gmail.com>
2023-03-26 17:32:57 -0300

    add: novas variáveis de posição e etc

--- Assets/config.h
+++ Assets/config.h
@@ -27,6 +27,12 @@
 #define TAM_MAP 10
 #define QUANT_TEX 2
 
+#define PLAYER_X 9.74f
+#define PLAYER_Z 24.65f
+#define PLAYER_LX 0.0f
+#define PLAYER_LZ -1.0f
+#define PLAYER_ANGLE 0.0f
+
 static GLfloat light_pos[] = {-2.0, 2.0, 2.0, 0.0};
 static GLfloat light_Ka[] = {0.4, 0.4, 0.4, 0.0};
 static GLfloat light_Kd[] = {1.0, 1.0, 1.0, 0.0};
@@ -37,5 +43,18 @@ static GLfloat material_Kd[] = {0.78, 0.57, 0.11, 1.00};
 static GLfloat material_Ks[] = {0.99, 0.94, 0.81, 1.00};
 static GLfloat material_Ke[] = {0.00, 0.00, 0.00, 0.00};
 static GLfloat material_Se = 28;
+
+static int map[TAM_MAP][TAM_MAP] = {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                                    {1, 0, 0, 0, 0, 1, 0, 0, 0, 1}, 
+                                    {1, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+                                    {1, 0, 1, 1, 1, 1, 0, 0, 0, 1},
+                                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                                    {1, 1, 1, 1, 1, 0, 1, 1, 0, 1},
+                                    {1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+                                    {1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+                                    {1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+                                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+                                    };
+
 
 #endif
