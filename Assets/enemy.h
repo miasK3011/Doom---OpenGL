b453f0a468c908e8db2e50d9c993201a8b17b1cd
miasK3011 <neemiascalebe2014@gmail.com>
2023-03-28 16:19:06 -0300

    add: Criação da classe Enemy

---
+++ Assets/enemy.h
@@ -0,0 +1,31 @@
+#ifdef __APPLE__
+#define GL_SILENCE_DEPRECATION
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#else
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
+
+#include <random>
+
+#ifndef enemy_h
+#define enemy_h
+
+class Enemy {
+    public:
+        Enemy(float x, float z);
+        void render();
+        void randMov();
+        float getPosx();
+        float getPosz();
+        float setPosx(float x);
+        float setPosz(float z);
+        float getRadius();
+    private:
+        int posx, posz, radius = 0.75f;
+};
+
+#endif
